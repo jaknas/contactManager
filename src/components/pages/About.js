50aeb6cf276a63f33593df1ff4ea34d4ca6bbe41
kleyu <jackob.naskreski@gmail.com>
2018-08-04 22:42:29 +0200

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default () => {
+	return (
+		<div>
+			<h1 className="display-4">About Contact Manager</h1>
+			<p className="lead">
+				A React based app to manage contacts, connected to API and with more
+				advanced state management.
+			</p>
+			<p className="text-secondary">Version 1.0.0</p>
+		</div>
+	);
+};
